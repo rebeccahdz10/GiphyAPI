9fdedcbb7a137cc7b13c9b1c0c905812b7344c0e
Rebecca Hernandez <rebeccahernandez@Earls-MacBook-Air.local>
2019-07-29 08:34:59 -0700

    added on click function

--- logic.js
+++ logic.js
@@ -13,15 +13,43 @@ var topics = [
     "Hibiscus",
     "Orchid"
 ]
-var userInput = "";
+// var userInput = "";
 
 
 //FUNCTIONS
 function createButtons() {
     for (var i = 0; i < topics.length; i++) {
         document.getElementById("buttonCont").innerHTML += "<button>" + topics[i] + "</button>";
+        $("<button>").attr("id", "flower");
     }
 }
+$("#flower").on("click", function () {
+    var plants = $(this)
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        plants + "&api_key=FEGUgBnb0ugsVKKqYGEX8N2f4cm4s81U&limit=15";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response.data;
+            var newDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var flowImage = $("<img>");
+
+            flowImage.attr("src", results[i].images.fixed_height.url);
+
+            newDiv.append(p);
+            newDiv.append(flowImage);
+
+            $("#giphyCont").prepend(gifDiv);
+        });
+
+});
+
 
 
 //MAIN PROCESS
