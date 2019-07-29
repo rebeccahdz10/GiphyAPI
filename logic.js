3796aa1a1b32da89f0b5f028c543a2c94e0f2ca3
Rebecca Hernandez <rebeccahernandez@Earls-MacBook-Air.local>
2019-07-29 11:12:19 -0700

    updated function to jquery

--- logic.js
+++ logic.js
@@ -13,44 +13,45 @@ var topics = [
     "Hibiscus",
     "Orchid"
 ]
-// var userInput = "";
+var userInput = "";
 
 
 //FUNCTIONS
-function createButtons() {
-    for (var i = 0; i < topics.length; i++) {
-        document.getElementById("buttonCont").innerHTML += "<button>" + topics[i] + "</button>";
-        $("<button>").attr("id", "flower");
-    }
+
+for (var i = 0; i < topics.length; i++) {
+    var buttons = $('<button>' + topics[i] + '</button>')
+    buttons.appendTo('#buttonCont');
 }
-$("#flower").on("click", function () {
-    var plants = $(this)
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        plants + "&api_key=FEGUgBnb0ugsVKKqYGEX8N2f4cm4s81U&limit=15";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            var results = response.data;
-            var newDiv = $("<div>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
-            var flowImage = $("<img>");
 
-            flowImage.attr("src", results[i].images.fixed_height.url);
+// $(".newButton").on("click", function () {
+//     var plants = $(this).val();
 
-            newDiv.append(p);
-            newDiv.append(flowImage);
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//         plants + "&api_key=FEGUgBnb0ugsVKKqYGEX8N2f4cm4s81U&limit=15";
 
-            $("#giphyCont").prepend(gifDiv);
-        });
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     })
+//         .then(function (response) {
+//             var results = response.data;
+//             var newDiv = $("<div>");
+//             var rating = results[i].rating;
+//             var p = $("<p>").text("Rating: " + rating);
+//             var flowImage = $("<img>");
 
-});
+//             flowImage.attr("src", results[i].images.fixed_height.url);
+
+//             newDiv.append(p);
+//             newDiv.append(flowImage);
+
+//             $("#giphyCont").prepend(gifDiv);
+//         });
+
+// });
 
 
 
 //MAIN PROCESS
-createButtons();
