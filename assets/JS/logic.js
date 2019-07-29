dc7f8818a0676977b6ec57337b9c81a35e6365f1
Rebeccahdz10 <rebeccahdz10@gmail.com>
2019-07-29 14:09:47 -0700

    added val to plants var

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -26,7 +26,7 @@ for (var i = 0; i < topics.length; i++) {
 
 
 $(".flowButton").on("click", function () {
-    var plants = topics[i];
+    var plants = $(this).val(topics[i]);
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         plants + "&api_key=FEGUgBnb0ugsVKKqYGEX8N2f4cm4s81U&limit=10";
@@ -36,12 +36,17 @@ $(".flowButton").on("click", function () {
         method: "GET"
     }).done(function (response) {
         console.log(response.data);
-        var imageUrl = response.data.image_original_url;
+
+        // var results = response.data;
+        var newDiv = $("<div>");
         var flowImage = $("<img>");
 
-        flowImage.attr("src", imageUrl);
+        flowImage.attr("src", response.images.downsized);
 
-        $("#giphyCont").prepend(flowImage);
+        // newDiv.append(p);
+        newDiv.append(flowImage);
+
+        $("#giphyCont").prepend(newDiv);
     })
 
     // .then(function (response) {
