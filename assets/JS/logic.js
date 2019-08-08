f321bbef51cea0dfb357ee5c45e0392016ff148f
Rebeccahdz10 <rebeccahdz10@gmail.com>
2019-08-08 15:20:42 -0700

    rating added

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -26,7 +26,7 @@ for (var i = 0; i < topics.length; i++) {
 
 
 $(".flowButton").on("click", function () {
-    var flor = $(this).text;
+    var flor = $(this).text();
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         flor + "&api_key=FEGUgBnb0ugsVKKqYGEX8N2f4cm4s81U&limit=10";
@@ -34,34 +34,30 @@ $(".flowButton").on("click", function () {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).done(function (response) {
-        console.log(response.data);
-
-        // var results = response.data;
-        var newDiv = $("<div>");
-        var flowImage = $("<img>");
-
-        flowImage.attr("src", response.images.downsized);
-
-        // newDiv.append(p);
-        newDiv.append(flowImage);
-
-        $("#giphyCont").prepend(newDiv);
     })
+        .then(function (response) {
+            console.log(response.data);
 
-    // .then(function (response) {
-    //     var results = response.data;
-    //     var newDiv = $("<div>");
-    //     var flowImage = $("<img>");
+            var results = response.data;
 
-    //     flowImage.attr("src", results[i].images.fixed_height.url);
+            for (var i = 0; i < results.length; i++) {
 
-    //     // newDiv.append(p);
-    //     newDiv.append(flowImage);
 
-    //     $("#giphyCont").prepend(newDiv);
-    // });
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    var newDiv = $("<div>");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var flowImage = $("<img>");
 
+                    flowImage.attr("src", results[i].images.fixed_height.url);
+
+                    newDiv.append(p);
+                    newDiv.append(flowImage);
+
+                    $("#giphyCont").prepend(newDiv);
+                }
+            }
+        });
 });
 
 
